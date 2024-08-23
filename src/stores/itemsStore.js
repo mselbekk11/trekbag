ebe6ce3d0e646231096dfa0882924da218317f43
mselbekk11 <mselbekk11@gmail.com>
2024-08-23 08:03:39 -0700

    ddd

--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -1,3 +1,20 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import { intialItems } from '../lib/constants';
 
-create();
+create((set) => ({
+  items: intialItems,
+  removeAllItems: () => {
+    set(() => ({ items: [] }));
+  },
+  resetToInitial: () => {
+    set(() => ({ items: intialItems }));
+  },
+  markAllAsComplete: () => {
+    set((state) => {
+      const newItems = state.items.map((item) => {
+        return { ...item, packed: true };
+      });
+      return { items: newItems };
+    });
+  },
+}));
